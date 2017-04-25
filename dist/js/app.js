b2849fceee93c8203cb16603f4abcdc876d284bc
Sarah Clements <biting.the.sun@gmail.com>
2017-04-25 14:05:45 -0700

    removed contributions onclick

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,7 +1,4 @@
 $(function () {
-    $('.contrib').on('click', function (event) {
-        $(event.target).parents().next('ul').fadeToggle('slow');
-    });
 
     $('.down-nav-arrow').on('click', function () {
 		document.querySelector('#portfolio-buffer').scrollIntoView({behavior: 'smooth'});
